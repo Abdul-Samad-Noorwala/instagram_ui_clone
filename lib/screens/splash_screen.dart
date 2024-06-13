283f292b96a68a9f5f15e263cb03dcb52b2ad0f5
Abdul Samad <145452813+Abdul-Samad-Noorwala@users.noreply.github.com>
2024-06-13 21:00:37 +0500

    Completed Splash Screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:instagram_ui_clone/screens/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,7 +11,42 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 5), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomeScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset(
+              'images/instagram_logo.png',
+              height: 350,
+            ),
+          ),
+          // const SizedBox(
+          //   height: 2,
+          // ),
+          const Text(
+            'Instagram UI',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+                fontStyle: FontStyle.italic),
+          )
+        ],
+      ),
+    );
   }
 }
